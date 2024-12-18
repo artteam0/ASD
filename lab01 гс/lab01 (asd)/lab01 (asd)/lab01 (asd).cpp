69c670087102a168443047301ee6d83d020e2dba
artteam0 <154960280+artteam0@users.noreply.github.com>
2024-12-18 19:36:42 +0300

    Add files via upload

---
+++ lab01 гс/lab01 (asd)/lab01 (asd)/lab01 (asd).cpp
@@ -0,0 +1,65 @@
+﻿//#include <iostream>
+//#include <chrono>
+//using namespace std;
+//
+//int number(int n)
+//{
+//    if (n < 2) return 1;
+//    return number(n - 2) + number(n - 1);
+//}
+//
+//int main()
+//{
+//    setlocale(LC_CTYPE, "rus");
+//
+//    float a, b;
+//
+//    int N, i;
+//    cout << "Введите количество чисел последовательности: ";
+//    cin >> N;
+//    
+//    a = clock();
+//    
+//    for (i = 0; i <= N; i++)
+//    {
+//        cout << number(i) << " ";
+//    }
+//
+//    cout << endl;
+//
+//    b = clock();
+//    cout << "Затраченное время: " << (b - a) / 1000 << "c" << endl;
+//
+//    return 0;
+//}
+
+
+
+#include <iostream>
+#include <chrono>
+using namespace std;
+
+void main()
+{
+    setlocale(LC_CTYPE, "rus");
+
+    float a, b;
+    long N;
+    long i, k = 0, i1 = 0, i2 = 1;
+    cout << "Введите количество чисел последовательности: ";
+    cin >> N;
+
+    a = clock();
+
+    while (k < N)
+    {
+        i = i1 + i2;
+        i1 = i2;
+        i2 = i;
+        k += 1;
+    }
+    cout << i2 << endl;
+
+    b = clock();
+    cout << "Затраченное время: " << (b - a) / 1000 << "с" << endl;
+}
---
+++ lab02 гс/lab02(asd)/lab02(asd)/lab02(asd).cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+void main()
+{
+	int firstNum, numForSearch, startNum, endNum, i, num, choice;
+
+	cout << "enter your num: ";
+	cin >> firstNum;
+	if (firstNum == NULL) {
+		cout << "gg" << endl;
+		exit(69);
+	}
+
+	srand(time(NULL));
+	rand();
+
+	numForSearch = rand() % firstNum + 1;
+	cout << endl << numForSearch;
+
+	startNum = 1;
+	endNum = firstNum;
+	i = 0;
+
+	while (true)
+	{
+		i += 1;
+		num = ((endNum - startNum) / 2 + startNum);
+
+		cout << endl << num << "?" << endl << "1.its my num" << endl << "2.is more" << endl << "3.is less" << endl;
+		cin >> choice;
+
+		switch (choice)
+		{
+		case 1:
+		{
+			cout << "you win!!" << endl << "count of max steps: " << int(log(firstNum) / log(2)) + 1 << endl << "count of steps: " << i;
+			exit(69);
+		}
+		case 2:
+		{
+			startNum = num;
+			break;
+		}
+		case 3:
+		{
+			endNum = num;
+			break;
+		}
+
+		}
+	}
+}
